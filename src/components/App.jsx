16716b4ebff594ae45bbdd54b4ac08ae910f452a
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-06-26 11:23:57 +0300

    Added files from privious hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import { Section } from './Section';
+import { Form } from './Form';
+import { Filter } from './Filter';
+import { ContactList } from './ContactList';
+import { Container } from './App.styled';
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Phonebook">
+        <Form />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
+    </Container>
   );
-};
+}
